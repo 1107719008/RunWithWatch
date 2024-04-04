fffcf54538300e1ad7a9013fc28e1bcd826e8689
Chun-Han-Lin <hanklin0208@gmail.com>
2024-04-04 16:42:59 +0800

    Initial Commit

---
+++ RunWithWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunWithWatch
+//
+//  Created by 林君翰 on 2024/4/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunWithWatch/RunWithWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunWithWatchApp.swift
+//  RunWithWatch
+//
+//  Created by 林君翰 on 2024/4/4.
+//
+
+import SwiftUI
+
+@main
+struct RunWithWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
