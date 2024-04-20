ea1ff4f242bda912de70bf82e64ee6bcd7293a55
Chun-Han-Lin <hanklin0208@gmail.com>
2024-04-20 16:06:58 +0800

    feat - add default ntue path

---
+++ WatchOSRunWithWatch Watch App/MapModelView.swift
@@ -0,0 +1,8 @@
+//
+//  MapModelView.swift
+//  WatchOSRunWithWatch Watch App
+//
+//  Created by 林君翰 on 2024/4/19.
+//
+
+import Foundation
---
+++ WatchOSRunWithWatch Watch App/RunningViews/Model/PathRecordModel.swift
@@ -0,0 +1,8 @@
+//
+//  PathRecordModel.swift
+//  WatchOSRunWithWatch Watch App
+//
+//  Created by 林君翰 on 2024/4/19.
+//
+
+import Foundation
