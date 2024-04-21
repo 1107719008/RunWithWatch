ae920a893f73a312947e4fe353c4ec883e9cdaf5
Chun-Han-Lin <hanklin0208@gmail.com>
2024-04-22 02:43:10 +0800

    feat - add ui and path card

---
+++ WatchOSRunWithWatch Watch App/ChooseFriendView.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseFriendView.swift
+//  WatchOSRunWithWatch Watch App
+//
+//  Created by 林君翰 on 2024/4/22.
+//
+
+import SwiftUI
+
+struct ChooseFriendView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseFriendView()
+}
